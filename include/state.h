7090502e8354da820878b62e2fb13d29d877b032
vasshorin <)tSz%gBWz1>
2023-01-15 13:41:17 -0800

    Add stdin,out,err to state

--- include/state.h
+++ include/state.h
@@ -9,6 +9,7 @@
 #include <regex.h>
 #include "command.h"
 #include <stdbool.h>
+#include <stdio.h>
 
 /*! \struct state
 *  \brief The state of the shell.
@@ -17,16 +18,19 @@
  */
 struct state
 {
+    FILE *stdin;                 /*!< The standard input stream. */
+    FILE *stdout;                /*!< The standard output stream. */
+    FILE *stderr;                /*!< The standard error stream. */
     regex_t *in_redirect_regex;  /*!< The regex for in redirection. */
     regex_t *out_redirect_regex; /*!< The regex for out redirection. */
     regex_t *err_redirect_regex; /*!< The regex for err redirection. */
-    char **path;                /*!< The PATH env var. */
-    char *prompt;               /*!< The PS1 env var. */
-    size_t max_line_length;     /*!< The max line length. */
-    char *current_line;         /*!< The current line. */
-    size_t current_line_length; /*!< The current line length. */
-    struct command *command;    /*!< The current command. */
-    bool fatal_error;           /*!< A fatal error has occurred. */
+    char **path;                 /*!< The PATH env var. */
+    char *prompt;                /*!< The PS1 env var. */
+    size_t max_line_length;      /*!< The max line length. */
+    char *current_line;          /*!< The current line. */
+    size_t current_line_length;  /*!< The current line length. */
+    struct command *command;     /*!< The current command. */
+    bool fatal_error;            /*!< A fatal error has occurred. */
 };
 
 #endif //DC_SHELL_STATE_H
